b8228a444d16977a98531b10334d44b464f56b00
adeyemo adekunle <adeyemokay@gmail.com>
2021-10-30 21:49:21 +0100

    ref: refactor code

--- frontend/src/components/cart-btn/cart-btn.tsx
+++ frontend/src/components/cart-btn/cart-btn.tsx
@@ -1,10 +1,17 @@
+import { useAppStateContext } from "../../context/state";
 import "./cart-btn.scss";
 const CartBtn = () => {
+  const { cart } = useAppStateContext();
   return (
-    <button className="btn">
-      <img src="/images/shopping-cart.svg" />
-      Cart
-    </button>
+    <div className="cart-btn">
+      <button className="btn">
+        <img src="/images/shopping-cart.svg" />
+        Cart
+        <div className="badge-container">
+          <span className="badge">{cart.length}</span>
+        </div>
+      </button>
+    </div>
   );
 };
 
--- frontend/src/components/cart/CartList.tsx
+++ frontend/src/components/cart/CartList.tsx
@@ -1,11 +1,9 @@
 import { FC } from "react";
-
+import "./cart-list.scss";
 const CartList: FC<unknown> = () => {
-    return (
-        <div className="cart-list">
-            
-        </div>
-    )
-}
+  return <div className="cart-list">
+      
+  </div>;
+};
 
 export default CartList;
